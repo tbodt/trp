1340238fe33414d848ca21f45cd5462b11644646
Theodore <tblodt@icloud.com>
2014-05-27 14:10:30 -0700

    I don't really need the Transformer

--- src/main/java/com/tbodt/puzzlesolver/PuzzleSolver.java
+++ src/main/java/com/tbodt/puzzlesolver/PuzzleSolver.java
@@ -8,8 +8,14 @@ package com.tbodt.puzzlesolver;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.BaseErrorListener;
 import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.RecognitionException;
+import org.antlr.v4.runtime.Recognizer;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
 
@@ -20,6 +26,8 @@ import org.antlr.v4.runtime.tree.ParseTreeWalker;
  */
 public class PuzzleSolver {
 
+    private static boolean errors;
+
     public static void main(String[] args) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         while (true) {
@@ -28,14 +36,34 @@ public class PuzzleSolver {
             if (input == null) {
                 break;
             }
+            errors = false;
             ANTLRInputStream inputStream = new ANTLRInputStream(input);
             PuzzleLexer lexer = new PuzzleLexer(inputStream);
             PuzzleParser parser = new PuzzleParser(new CommonTokenStream(lexer));
+            parser.addErrorListener(new BaseErrorListener() {
+                @Override
+                public void syntaxError(Recognizer<?, ?> rcgnzr, Object o, int i, int i1, String string, RecognitionException re) {
+                    errors = true;
+                }
+            });
             ParseTree tree = parser.puzzle();
             ParseTreeWalker walker = new ParseTreeWalker();
             PuzzleParseListener listener = new PuzzleParseListener();
             walker.walk(listener, tree);
-            System.out.println(Transformer.transform(listener.getData(), listener.getTransformations()));
+            if (errors == false) {
+                System.out.println(transform(listener.getData(), listener.getTransformations()));
+            }
         }
+    }
+
+    public static Set<String> transform(Set<String> data, List<Transformation> transformations) {
+        for (Transformation tx : transformations) {
+            Set<String> newData = new HashSet<>();
+            for (String datum : data) {
+                newData.addAll(tx.transform(datum));
+            }
+            data = newData;
+        }
+        return data;
     }
 }
