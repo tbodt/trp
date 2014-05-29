0e9b10611584ae48a545a5471afa20227d12f862
Theodore <tblodt@icloud.com>
2014-05-29 16:20:14 -0700

    We now have a Function class!

--- src/main/java/com/tbodt/puzzlesolver/Function.java
+++ src/main/java/com/tbodt/puzzlesolver/Function.java
@@ -3,15 +3,50 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.tbodt.puzzlesolver;
 
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  *
  * @author Theodore Dubois
  */
 public class Function {
-    private Method method;
+    private final Method method;
+    private static final Map<String, Function> cache = new HashMap<>();
+
+    private Function(String name) throws NoSuchMethodException {
+        Method[] methods = Function.class.getMethods();
+        Method mt = null;
+        for (Method m : methods) {
+            if (!Modifier.isStatic(m.getModifiers()))
+                continue;
+            if (Modifier.isPrivate(m.getModifiers()))
+                continue;
+            if (m.getName().equals("forNameAndArgs"))
+                continue;
+            if (m.getName().equals(name))
+                mt = m;
+        }
+        if (mt != null)
+            method = mt;
+        else
+            throw new NoSuchMethodException(name);
+    }
+
+    public static Function forNameAndArgs(String name) {
+        if (cache.containsKey(name))
+            return cache.get(name);
+        try {
+            Function f = new Function(name);
+            cache.put(name, f);
+            return f;
+        } catch (NoSuchMethodException ex) {
+            return null;
+        }
+    }
+
 }
