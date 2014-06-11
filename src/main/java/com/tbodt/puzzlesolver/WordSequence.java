69aaa50e5eef0029ec798d71ea402062721c2e39
Theodore Dubois <tblodt@icloud.com>
2014-06-11 14:14:05 -0700

    finish up the forEachWord method

--- src/main/java/com/tbodt/puzzlesolver/WordSequence.java
+++ src/main/java/com/tbodt/puzzlesolver/WordSequence.java
@@ -21,7 +21,11 @@ public final class WordSequence implements Iterable<WordSequence.Word> {
     public WordSequence(String data) {
         words = Pattern.compile("\\s+").splitAsStream(data).map(Word::new).collect(Collectors.toList());
     }
-    
+
+    public WordSequence(Word datum) {
+        words = Collections.singletonList(datum);
+    }
+
     public WordSequence(List<Word> data) {
         words = new ArrayList<>(data);
     }
@@ -33,11 +37,11 @@ public final class WordSequence implements Iterable<WordSequence.Word> {
     public WordSequence(Stream<Word> data) {
         words = data.collect(Collectors.toList());
     }
-    
+
     public List<Word> getWords() {
         return Collections.unmodifiableList(words);
     }
-    
+
     public static class Word implements CharSequence {
         private final String word;
 
@@ -51,7 +55,7 @@ public final class WordSequence implements Iterable<WordSequence.Word> {
         public char charAt(int index) {
             return word.charAt(index);
         }
-        
+
         @Override
         public CharSequence subSequence(int beginIndex, int endIndex) {
             return word.subSequence(beginIndex, endIndex);
@@ -78,6 +82,7 @@ public final class WordSequence implements Iterable<WordSequence.Word> {
         public String toString() {
             return word;
         }
+
     }
 
     public WordSequence append(Word word) {
@@ -85,11 +90,11 @@ public final class WordSequence implements Iterable<WordSequence.Word> {
         wordsList.add(word);
         return new WordSequence(wordsList);
     }
-    
+
     public WordSequence combine() {
         return new WordSequence(String.join("", words));
     }
-    
+
     @Override
     public Iterator<Word> iterator() {
         return Collections.unmodifiableList(words).iterator();
@@ -109,10 +114,18 @@ public final class WordSequence implements Iterable<WordSequence.Word> {
     public String toString() {
         return String.join(" ", words);
     }
-    
+
     public static java.util.function.Function<WordSequence, Stream<WordSequence>> forEachWord(java.util.function.Function<Word, Stream<Word>> function) {
-        // This code is complicated, but it simply performs the operation on the first word and discards the rest. It's only temporary.
-        return ws -> function.apply(ws.getWords().get(0)).map(w -> new WordSequence(Collections.singletonList(w)));
+        return ws -> {
+            Stream<WordSequence> result = null;
+            for (Word word : ws) {
+                Stream<Word> transformed = function.apply(word);
+                if (result == null)
+                    result = transformed.map(WordSequence::new);
+                result = result.flatMap(seq -> transformed.map(w -> seq.append(w)));
+            }
+            return result;
+        };
     }
 
 }
