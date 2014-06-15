5795eb47ba4f01fb0f13a05990662163c0e60155
Theodore Dubois <tblodt@icloud.com>
2014-06-15 16:06:40 -0700

    implement much better anagram algorithm

--- src/main/java/com/tbodt/puzzlesolver/TransformerFunction.java
+++ src/main/java/com/tbodt/puzzlesolver/TransformerFunction.java
@@ -8,6 +8,7 @@ package com.tbodt.puzzlesolver;
 import com.tbodt.puzzlesolver.WordSequence.Word;
 import java.util.*;
 import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 /**
  *
@@ -29,7 +30,7 @@ public class TransformerFunction {
         Stream<WordSequence> invoke(Stream<WordSequence> data, Object[] parameters);
 
     }
-    
+
     private TransformerFunction(Transformer lambda) {
         this(lambda, new ArgumentList());
     }
@@ -64,19 +65,61 @@ public class TransformerFunction {
         }
 
         public static Stream<WordSequence> anagram(Stream<WordSequence> data, Object[] parameters) {
-            return data.unordered().flatMap(WordSequence.forEachWord(Transformers::allAnagrams)).distinct();
-        }
+            class AnagramIterator implements Iterator<Word> {
+                private final Word start;
+                private Word current;
+                private boolean done;
 
-        private static Stream<Word> allAnagrams(Word data) {
-            if (data.length() <= 1)
-                return Stream.of(data);
-            Stream<Word> ret = Stream.empty();
-            for (int i = 0; i < data.length(); i++) {
-                char ch = data.charAt(i);
-                String rest = new StringBuilder(data).deleteCharAt(i).toString();
-                ret = Stream.concat(ret, allAnagrams(new Word(rest)).map(word -> new Word(ch + word.toString()))).unordered();
+                AnagramIterator(Word start) {
+                    current = this.start = start;
+                }
+
+                @Override
+                public boolean hasNext() {
+                    return !done;
+                }
+
+                @Override
+                public Word next() {
+                    if (done)
+                        throw new NoSuchElementException();
+                    StringBuilder b = new StringBuilder(current);
+                    for (int i = b.length() - 1; i > 0; i--)
+                        if (b.charAt(i - 1) < b.charAt(i)) {
+                            int j = b.length() - 1;
+                            while (b.charAt(i - 1) > b.charAt(j))
+                                j--;
+                            swap(b, i - 1, j);
+                            reverse(b, i);
+                            current = new Word(b.toString());
+                            done = current.equals(start);
+                            return current;
+                        }
+                    current = new Word(b.reverse().toString());
+                    done = current.equals(start);
+                    return current;
+                }
+
+                private void swap(StringBuilder b, int i, int j) {
+                    char tmp = b.charAt(i);
+                    b.setCharAt(i, b.charAt(j));
+                    b.setCharAt(j, tmp);
+                }
+
+                private void reverse(StringBuilder b, int i) {
+                    int j = b.length() - 1;
+                    while (i < j) {
+                        swap(b, i, j);
+                        i++;
+                        j--;
+                    }
+                }
             }
-            return ret;
+            return data.flatMap(WordSequence.forEachWord(w -> StreamSupport.stream(
+                    Spliterators.spliterator(
+                            new AnagramIterator(w), Long.MAX_VALUE,
+                            Spliterator.DISTINCT + Spliterator.IMMUTABLE + Spliterator.NONNULL),
+                    false)));
         }
 
         private Transformers() {
