a65bce7f721a0f438c0ba279467e50c8ee751d1b
Theodore Dubois <tblodt@icloud.com>
2014-07-31 17:31:12 -0700

    put in command line parser thing

--- src/main/java/com/tbodt/trp/TheRapidPermuter.java
+++ src/main/java/com/tbodt/trp/TheRapidPermuter.java
@@ -18,32 +18,76 @@
 package com.tbodt.trp;
 
 import java.io.*;
+import java.util.Arrays;
+import org.apache.commons.cli.*;
 
 /**
  * Main class for The Rapid Permuter.
  *
  * @author Theodore Dubois
  */
+@SuppressWarnings("static-access") // the builder for the commons cli is poorly designed
 public class TheRapidPermuter {
 
+    private static final Option command = OptionBuilder
+            .withLongOpt("command")
+            .hasArg()
+            .withDescription("Command to execute")
+            .create('c');
+    private static final Option timing = OptionBuilder
+            .withLongOpt("timing")
+            .withDescription("Time command executions in milliseconds")
+            .create('t');
+    private static final Option help = OptionBuilder
+            .withLongOpt("help")
+            .withDescription("Display command line option help")
+            .create();
+    private static final Options options = new Options()
+            .addOption(command)
+            .addOption(timing)
+            .addOption(help);
+    private static CommandLine cmd;
     /**
      * Main method for The Rapid Permuter.
      * 
      * @param args
      * @throws IOException
+     * @throws org.apache.commons.cli.ParseException
      */
-    public static void main(String[] args) throws IOException {
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        System.out.print("trp> ");
-        String input;
-        while ((input = in.readLine()) != null) {
-            if (input.equals("exit"))
-                return; // exit
-            // The most important line in the program!
-            CommandProcessor.processCommand(input).ifPresent(data ->
-                    data.forEach(System.out::println));
-            System.gc(); // why not?
-            System.out.print("trp> ");
+    public static void main(String[] args) throws IOException, ParseException {
+        cmd = new BasicParser().parse(options, args);
+        if (Arrays.asList(cmd.getOptions()).contains(help)) { // another way commons cli is severely broken
+            new HelpFormatter().printHelp("trp", options);
+            return;
         }
+        
+        if (cmd.hasOption('c')) {
+            System.out.println(cmd.getOptionValue('c'));
+            doCommand(cmd.getOptionValue('c'));
+        }
+        else {
+            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+            System.out.print("trp> ");
+            String input;
+            while ((input = in.readLine()) != null) {
+                if (input.equals("exit"))
+                    return; // exit
+                doCommand(input);
+                System.out.print("trp> ");
+            }
+        }
+    }
+    
+    private static void doCommand(String command) {
+        long before = System.nanoTime();
+        
+        // The most important line in the program!
+        CommandProcessor.processCommand(command).ifPresent(data ->
+                data.forEach(System.out::println));
+        
+        long after = System.nanoTime();
+        if (cmd.hasOption('t'))
+            System.out.println ((after - before)/1_000_000 + "ms");
+        System.gc(); // why not?
     }
 }
