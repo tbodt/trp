01de57aff36d2824d1eb352a29d35f268d5b7e14
Theodore Dubois <tblodt@icloud.com>
2014-08-28 16:55:35 -0700

    actually, it's working fine, except for mismatched parentheses

--- src/test/java/com/tbodt/trp/IntegrationTest.java
+++ src/test/java/com/tbodt/trp/IntegrationTest.java
@@ -30,7 +30,7 @@ public class IntegrationTest {
     public void testCommands() {
         testCommand("\"hi\"", new String[] {"hi"});
         testCommand("[testCategory]", new String[] {"test", "another test", "yet another test"});
-        testCommand("[testCategory]: all(in([testCategory])", new String[] {"test"});
+        testCommand("[testCategory]: all(in([testCategory]))", new String[] {"test"});
     }
     
     @Test
